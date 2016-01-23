a1293c0f37c21bc810ae154f123b6b8b08da5909
Mathias Buus <mathiasbuus@gmail.com>
2016-01-24 00:25:18 +0100

    add open.sync

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var fs = require('fs')
 var c = require('constants')
 
+var mode = c.O_RDWR | c.O_CREAT
+
+open.sync = sync
 module.exports = open
 
 function open (filename, cb) {
-  var mode = c.O_RDWR | c.O_CREAT
   fs.open(filename, mode, cb)
+}
+
+function sync (filename, cb) {
+  return fs.openSync(filename, mode)
 }
